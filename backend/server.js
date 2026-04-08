3ae5425c9fd2c1422d9f9d779e5e7e8211f39f16
Sujal Verma <e22cseu1464@bennett.edu.in>
2026-04-08 18:10:48 +0530

    Update: Default port to 10000

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Server is running on port ' + `${PORT}`);
 });
